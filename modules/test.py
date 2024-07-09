9cf0eb5a06b8e63c4200e8fc9039f7fde4c4a306
uddin2 <md.borhan.uddin@uni-potsdam.de>
2024-07-09 21:03:12 +0200

    Added missing value functions

--- modules/test.py
+++ modules/test.py
@@ -8,3 +8,31 @@ from modules.modules import (
     drop_columns_with_zero_threshold,
     separate_categorical_numerical,
 )
+
+# Tests for count_null_data function
+
+
+def test_missing_values(capsys):
+    """
+    Check if the function prints the correct output
+    when there are missing values
+    """
+    data = pd.DataFrame({"A": [1, None, 5, 8], "B": [5, None, 5, None]})
+    count_null_data(data)
+    captured = capsys.readouterr()
+    assert (
+        captured.out.strip()
+        == "Column 'A': 1 values 0\nColumn \
+'B': 2 values 0"
+    )
+
+
+def test_no_missing_values(capsys):
+    """
+    Check if the function prints the correct
+    output when there are no missing values
+    """
+    data = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+    count_null_data(data)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "There are no 0 value anymore!"
