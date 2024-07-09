92eaa41bc8e96b1c1e5e3b5367cf21d3890f0970
Ahmed <UI687576@rwe.com>
2024-07-09 20:44:09 +0200

    created test file

---
+++ modules/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+from modules.modules import (
+    apply_1_plus_log_transformation,
+    count_null_data,
+    delete_columns_with_zero_data,
+    drop_columns_with_zero_threshold,
+    separate_categorical_numerical,
+)
