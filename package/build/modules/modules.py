ba7c88df8432d3d21e76036b49239407e7d92241
uddin2 <md.borhan.uddin@uni-potsdam.de>
2024-07-14 16:27:26 +0200

    Added rest of the code and Finalize the modules

--- package/build/modules/modules.py
+++ package/build/modules/modules.py
@@ -130,3 +130,33 @@ def plot_heatmaps(df):
     ax[1].set_title('Top 10 most correlated variables with sale price')
     plt.savefig("results/plot_preprocessing/Correlation Matrix Heatmap.png")
     plt.show()
+    
+
+def hyperparameter_tuning(models, param_grids, X_train, y_train):
+    """
+        Perform hyperparameter tuning using GridSearchCV for multiple models.
+        Parameters:
+        models: List of tuples where each tuple contains the model name (str) and the model instance.
+        param_grids: List of dictionaries with parameter names (str) as keys and lists of parameter settings to try as values.
+        X_train: Training data features.
+        y_train: Training data labels.
+        Returns:
+        best_models: Dictionary with model names as keys and the best found models as values.
+        best_params: Dictionary with model names as keys and the best found parameters as values.
+    """
+    best_models = {}
+    best_params = {}
+
+    for (name, model), param_grid in zip(models, param_grids):
+        print(f"Tuning hyperparameters for {name}...")
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error',
+                                   n_jobs=-1, verbose=2)
+        grid_search.fit(X_train, y_train)
+
+        best_models[name] = grid_search.best_estimator_
+        best_params[name] = grid_search.best_params_
+
+        print(f"Best parameters for {name}: {grid_search.best_params_}")
+
+    return best_models, best_params
+
