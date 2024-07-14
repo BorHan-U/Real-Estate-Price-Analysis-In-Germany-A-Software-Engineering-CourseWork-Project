264fa3f7978094fcbc34993ef863590cc6f59678
Ahmed <UI687576@rwe.com>
2024-07-14 14:45:04 +0200

    updated setup.py

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ setup(
     name='real estate price analysis',
     version='0.1.0',
     author=['Jayed Akbar Sumon ', 'Mohammed Hasan','Md Borhan Uddin', 'Aijaz Afzaal Ahmed','Md Raju Ahmed'],
-    author_email=['sumon@uni-potsdam.de', '@uni-potsdam.de',"@uni-potsdam.de","@uni-potsdam.de", "@uni-potsdam.de"],
+    author_email=['sumon@uni-potsdam.de', 'mohammad.hasan@uni-potsdam.de', 'md.borhan.uddin@uni-potsdam.de', 'aijazafzaal.ahmed@uni-potsdam.de', 'ahmed10@uni-potsdam.de'],
     description='Real estate price analysis using various machine learning techniques',
     long_description="The aim of this project is to explore the train dataset by performing preprocessing operations, including data mining techniques like removing irrelevant data and updating missing values. Subsequently, we will apply various machine learning algorithms to the preprocessed dataset. The ultimate goal is to determine the best model for predicting property prices based on their features, including factors like location.",
     url='https://gitup.uni-potsdam.de/sumon/real-estate-price-analysis',
